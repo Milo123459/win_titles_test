59f8be2dd6651556fea11c87d08212d3c2f55fce
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-15 14:06:30 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use window_titles::{Connection, ConnectionTrait};
+fn main() {
+	let connection = Connection::new().unwrap();
+	connection.windows().into_iter().filter(|w| w.process().name() == "Spotify.exe")
+        .filter(|s| !s.name().unwrap().trim().is_empty())
+		.for_each(|x| println!("{}", (x.name().unwrap())))
+}
