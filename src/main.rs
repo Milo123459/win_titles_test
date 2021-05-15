9def61d12b0d7af4386a8b9f644da9d8c08d51ee
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-15 14:06:55 +0100

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use window_titles::{Connection, ConnectionTrait};
 fn main() {
-	let connection = Connection::new().unwrap();
-	connection.windows().into_iter().filter(|w| w.process().name() == "Spotify.exe")
+    let connection = Connection::new().unwrap();
+    connection
+        .windows()
+        .into_iter()
+        .filter(|w| w.process().name() == "Spotify.exe")
         .filter(|s| !s.name().unwrap().trim().is_empty())
-		.for_each(|x| println!("{}", (x.name().unwrap())))
+        .for_each(|x| println!("{}", (x.name().unwrap())))
 }
